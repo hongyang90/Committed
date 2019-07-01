31b1cf12cf58b295faa7967e1ba84b80ac979805
HongYang Gao <hongyang.gao@gmail.com>
2019-07-01 15:41:52 -0700

    Setup initial outline for searching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.css';
+import Repos from './Components/Repos';
 
 class App extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      repos: [],
+      commits: []
+    };
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  handleSubmit(){
+
+  }
+
+  updateInput(){
+
+  }
+
 
   render () {
     return (
@@ -10,6 +27,11 @@ class App extends React.Component {
         <h1>
           Committed App
         </h1>
+        <div>
+          <input type="text" onChange={this.updateInput} placeholder='Enter a organization ie: facebook'/>
+          <button>Search</button>
+        </div>
+        <Repos />
       </div>
     );
 
